8eef962f43738809cff4dd1106df863b9c31db89
pablo <pablo.tolosa@gmail.com>
2023-09-05 22:39:04 -0300

    Clase 3

--- ejemplo.js
+++ ejemplo.js
@@ -71,36 +71,115 @@
 
 // <div>{`${ nombre } ${ apellido }`}</div>
 
-class Persona{
+// class Persona{
 
 
-    constructor(nombre){
-        this.nombre = nombre;
-    }
+//     constructor(nombre){
+//         this.nombre = nombre;
+//     }
     
     
-    saludar = () => {
-        console.log('Hola mi nombre es:' ,this.nombre);
-    }
-    getespecie = (  )=>{
-        let especie = 'humano';
-        console.log('Soy un ' ,especie);
-    }
-    
-    
-    
-    }
-    
-    let persona = new Persona('Esteban');
-    let persona2 = new Persona('Jorge');
-    let persona3 = new Persona('Jorge');
-    
-    persona.saludar();
-    persona.getespecie();
-    persona2.saludar();
-    persona2.getespecie();
-    persona3.saludar();
+//     saludar = () => {
+//         console.log('Hola mi nombre es:' ,this.nombre);
+//     }
+//     getespecie = (  )=>{
+//         let especie = 'humano';
+//         console.log('Soy un ' ,especie);
+//     }
     
     
     
+//     }
+    
+//     let persona = new Persona('Esteban');
+//     let persona2 = new Persona('Jorge');
+//     let persona3 = new Persona('Jorge');
+    
+//     persona.saludar();
+//     persona.getespecie();
+//     persona2.saludar();
+//     persona2.getespecie();
+//     persona3.saludar();
+    
+    
+    
+
+// let numeros = [1,2,3,4,5,6];
+
+// let totales = numeros.map((numero,indice)=>numero**indice);
+// console.log(totales);
+
+// let nombre = ['juan','pedro','maria', 'jose', 'gabriela'];
+
+
+// if (nombre.includes('jorge')){
+
+//   console.log('Existe en el array');
+
+// } else {
+
+//   console.log('No existe en el array');
+// }
+
+// let impuestos = {
+//   impuesto1:"2356",
+//   impuesto2:"562",
+//   impuesto3:"564",
+//   impuesto4:"2"
+// }
+// let parValor = Object.entries(impuestos);
+// console.log(parValor);
+
+// let soloPropiedades = Object.keys(impuestos);
+// console.log(soloPropiedades);
+
+// let soloValores = Object.values(impuestos);
+// console.log(soloValores);
+
+// let objeto1 = {
+//   propieda1: 2,
+//   propieda2: 62,
+//   propieda3: "b",
+//   propieda4: true
+// }
+// let objeto2 = {
+//   propieda1: "c",
+//   propieda2: [1,2,3,4]
+// }
+
+// let {propieda1, propieda3} = objeto1;
+
+// console.log(propieda3);
+
+let cadena = `      hola   `;
+console.log(cadena);
+console.log(cadena.trim());
+
+let mensaje = [];
+
+let intentoDeMensaje = ` hola     `;
+
+if(intentoDeMensaje.trim().length >0 ){
+
+mensaje.push(intentoDeMensaje.trim());
+
+} else {
+
+  console.log('mensaje vacio');
+}
+console.log(mensaje);
+
+let arrayAnidado = [1,365,[1,2,3],"b",["a","b"]];
+console.log(arrayAnidado.flat());
+
+
+let variablePrueba = 0;
+
+let variable = variablePrueba || "Sin valor";
+
+console.log(variable);
+
+let variableNullish = variablePrueba ?? "Sin valor";
+console.log(variableNullish);
+
 
